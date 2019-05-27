da2a849f35288469da2f469d231224c4a5d23870
Rodrigo César <rcdl@cin.ufpe.br>
2019-05-26 21:18:39 -0300

    Home screen layout

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 
+import { AppointmentEntry } from '../models/appoinment-entry';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+
+  entries: AppointmentEntry[] = [
+    {
+      expireTime: 10,
+      patientName: 'Regino',
+      patientAge: 23,
+      specialty: 'Odontologia',
+      paymentMethod: 'Plano Amil Dental',
+      date: 'Segunda, 27/05 às 14:00',
+    },
+  ];
 
   constructor() { }
 
---
+++ src/app/models/appoinment-entry.ts
@@ -0,0 +1,8 @@
+export class AppointmentEntry {
+    expireTime: number;
+    patientName: string;
+    patientAge: number;
+    specialty: string;
+    paymentMethod: string;
+    date: string;
+}
