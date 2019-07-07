d0adfcec48b515816ecc7da3600b9abd02c61af5
Rodrigo César Diniz e Lima <rcdl@cin.ufpe.br>
2019-07-06 21:01:34 -0300

    Delete unused files + fix base url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiRoot: 'http://ec2-3-16-1-205.us-east-2.compute.amazonaws.com/api/'
+  apiRoot: 'https://ec2-3-16-1-205.us-east-2.compute.amazonaws.com/api/'
 };
